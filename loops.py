4b412be29c1930a58e6ea8e0bc9d0c71e7840e28
Edouard <edouardtuyubahe@gmail.com>
2026-04-19 11:10:14 +0200

    Doing more complex loops and finalising the logic in patterns

--- loops.py
+++ loops.py
@@ -5,3 +5,17 @@ while a<11:
 
 for b in range(0,11):
    print(b) 
+
+for c in range(1, 21):
+   if c % 2 = 0:
+      print(c)   
+
+#or
+for c in range(2, 21, 2):
+    print(c)
+
+
+d = 1
+for d > 6:
+   print("*" * d)
+   d = d+1
