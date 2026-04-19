9c0ccd47cbfee395444fa565f02ab3bf6493c9f9
Edouard <edouardtuyubahe@gmail.com>
2026-04-19 12:24:22 +0200

    Finalising more about functions creating ones and diving into built in ones

---
+++ functions.py
@@ -0,0 +1,14 @@
+# printing hello to a name
+def greet(name):
+    print(f"Hello {name}")
+
+#Adding two numbers
+def add(a,b):
+    return a+b
+print(add(3,5))  
+
+
+# checking if even
+def is_even(b):
+    return b % 2 == 0
+print(is_even(5))
